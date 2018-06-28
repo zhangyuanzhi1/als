3838da375741c2c2975b12b98e287c19bf4971a7
zhangyuanzhi1 <zhangyuanzhi@ezbuy.com>
2018-06-28 14:07:03 +0800

    update

--- src/main/java/com/als/account/permission/config/WebMvcConfig.java
+++ src/main/java/com/als/account/permission/config/WebMvcConfig.java
@@ -6,7 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
-//tianxia wudi
+//tianxia wuditianxia wudi
+	
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
